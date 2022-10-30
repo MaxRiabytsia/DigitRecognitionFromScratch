deb2409ab90e514b4c63cef2ccbc7b874bea2bd1
Max <riabytsa@gmail.com>
2022-10-30 16:41:30 +0200

    Added noise to images and changed evaluation accordingly.

--- main.py
+++ main.py
@@ -5,22 +5,36 @@ import os
 from NeuralNetwork import NeuralNetwork
 
 
-def get_data() -> tuple[np.array, np.array, np.array, np.array]:
+def apply_noise(pixels):
+    pixels += np.random.uniform(-0.3, 0.3, pixels.shape)
+    pixels = (pixels - np.min(pixels)) / (np.max(pixels) - np.min(pixels))
+    return pixels
+
+
+def get_data() -> tuple[np.array, np.array, np.array, np.array, np.array, np.array]:
     test_size = 0.3
-    train_images, train_labels, test_images, test_labels = [], [], [], []
+    clean_test_size = test_size / 2
+    train_images, train_labels, test_images, test_labels, noisy_test_images, noisy_test_lables = [], [], [], [], [], []
     for folder in os.listdir("data/trainingSet"):
         images = os.listdir(f"data/trainingSet/{folder}")
-        for i, image_name in enumerate(images[:1000]):
+        for i, image_name in enumerate(images):
             image_path = f"data/trainingSet/{folder}/{image_name}"
-            pixels = cv2.imread(image_path, 0).flatten()
-            if i < len(images) * test_size:
+            pixels = cv2.imread(image_path, 0).flatten() / 255.
+            if i > len(images) * test_size:
+                if i % 2 == 0:
+                    pixels = apply_noise(pixels)
                 train_images.append(pixels)
-                train_labels.append(folder)
+                train_labels.append(int(folder))
+            elif len(images) * clean_test_size < i < len(images) * test_size:
+                pixels = apply_noise(pixels)
+                noisy_test_images.append(pixels)
+                noisy_test_lables.append(int(folder))
             else:
                 test_images.append(pixels)
-                test_labels.append(folder)
+                test_labels.append(int(folder))
 
-    return np.array(train_images), np.array(train_labels), np.array(test_images), np.array(test_labels)
+    return (np.array(train_images), np.array(train_labels), np.array(test_images),
+            np.array(test_labels), np.array(noisy_test_images), np.array(noisy_test_lables))
 
 
 def get_neural_net(train_images: np.array, train_labels: np.array) -> NeuralNetwork:
@@ -29,15 +43,22 @@ def get_neural_net(train_images: np.array, train_labels: np.array) -> NeuralNetw
         hidden_layer_neurons_number=32,
         output_layer_neurons_number=10
     )
-    neural_net.train(train_images, train_labels, epochs=10, batch_size=128, learning_rate=0.05)
+    neural_net.train(train_images, train_labels, epochs=2000, learning_rate=0.1)
     return neural_net
 
 
-def evaluate_neural_network(neural_net: NeuralNetwork, test_images: np.array, test_labels: np.array) -> None:
+def evaluate_neural_network(neural_net: NeuralNetwork, test_images: np.array, test_labels: np.array,
+                            noisy_test_images: np.array, noisy_test_lables: np.array) -> None:
     if not neural_net:
         print("Cannot evaluate until the neural network is trained.")
         return None
+    print("Evaluation results for all images: ")
+    neural_net.evaluate(np.concatenate([test_images, noisy_test_images]),
+                        np.concatenate([test_labels, noisy_test_lables]))
+    print("Evaluation results for clean images: ")
     neural_net.evaluate(test_images, test_labels)
+    print("Evaluation results for images with noise: ")
+    neural_net.evaluate(noisy_test_images, noisy_test_lables)
 
 
 def recognize_digit_from_img(neural_net: NeuralNetwork) -> None:
@@ -45,14 +66,19 @@ def recognize_digit_from_img(neural_net: NeuralNetwork) -> None:
         print("Cannot recognize digits until the neural network is trained.")
         return None
 
-    # img_path = input("Enter image path: ")
-    img_path = "data/trainingSample/0/img_1.jpg"
-    pixels = cv2.imread(img_path, 0)
-    cv2.imshow('Image', cv2.resize(cv2.imread(img_path, 0), (256, 256)))
+    img_path = input("Enter image path: ")
+    img_path = "data/trainingSample/" + img_path
+    pixels = cv2.imread(img_path, 0) / 255.
+
+    to_apply_noise = input("Do you want to apply noise to the image? (y/n)\n")
+    if to_apply_noise.lower() == 'y':
+        pixels = apply_noise(pixels)
+
+    cv2.imshow('Image', cv2.resize(pixels, (256, 256)))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    digit, probability = neural_net.predict(pixels.flattened)
-    print(f"The neural network is {probability * 100}% sure that the digit on the image is '{digit}'")
+    digit, probability = neural_net.predict(pixels.flatten())
+    print(f"The neural network is {round(probability * 100, 2)}% sure that the digit on the image is '{digit}'")
 
 
 def main():
@@ -60,7 +86,7 @@ def main():
           "ІПЗ-21\n")
 
     neural_net = None
-    train_images, train_labels, test_images, test_labels = get_data()
+    train_images, train_labels, test_images, test_labels, noisy_test_images, noisy_test_lables = get_data()
     while True:
         print("\n1. Train neural network\n"
               "2. Evaluate neural network\n"
@@ -68,11 +94,12 @@ def main():
               "0. Exit\n")
 
         task_number = int(input("Enter task number: "))
+        print()
         match task_number:
             case 1:
                 neural_net = get_neural_net(train_images, train_labels)
             case 2:
-                evaluate_neural_network(neural_net, test_images, test_labels)
+                evaluate_neural_network(neural_net, test_images, test_labels, noisy_test_images, noisy_test_lables)
             case 3:
                 recognize_digit_from_img(neural_net)
             case 0:
